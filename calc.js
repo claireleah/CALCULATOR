378dc98cfb309f4ffe317e983af748099bc4315b
claireleah <claireleah256@gmail.com>
2025-07-29 14:03:49 +0300

    disable buttons after an error

--- calc.js
+++ calc.js
@@ -1,23 +1,35 @@
 const display = document.getElementById('display');
+let isError = false;
 function appendToDisplay(input){
+    if (isError) return;
     display.value += input;
 
 }
 
 function clearDisplay(){
     display.value = '';
+    isError = false;
 
 }
 
 function calculate(){
     try{
-        display.value = eval(display.value);
+        let result = eval(display.value);
+
+        if (result === Infinity || result === -Infinity || isNaN(result)){
+            throw new Error();
+        }
+
+        display.value = result;
 
     }
 
     catch(error){
         display.value = 'Error';
+        isError = true;
     }
 
 }
+
+
 
